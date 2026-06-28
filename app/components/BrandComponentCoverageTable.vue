0a563a6bce67e05899d6aa471499d4b5d9f71b90
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-06-28 07:27:29 +0200

    refactor: use id coverage table directly

--- app/components/BrandComponentCoverageTable.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { happydesignsComponentCoverage } from '../utils/brand-guide'
-</script>
-
-<template>
-  <IdComponentCoverageTable :items="happydesignsComponentCoverage" />
-</template>
