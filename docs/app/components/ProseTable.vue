1600d09782351e14b356f5b69f695dea0097b7c1
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-08-19 22:49:01 +0200

    fix(docs): align prose tables with Nuxt UI

--- docs/app/components/ProseTable.vue
+++ docs/app/components/ProseTable.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="brand-table-scroll">
-    <table>
+  <div class="brand-table-scroll relative my-5 overflow-x-auto rounded-md outline-primary/25 focus-visible:outline-3">
+    <table class="w-full border-separate border-spacing-0 rounded-md">
       <slot />
     </table>
   </div>
